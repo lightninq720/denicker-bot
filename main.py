384ad01eb2e5bdbdbc139bda4317a5fb5d676120
_lightninq <82113835+lightninq720@users.noreply.github.com>
2023-03-21 22:04:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import nextcord
 import json
 import os
-from cogs.tickets import TicketsView, TicketManagementView, FAQView
-from cogs.rolemenu import RoleMenuView
 from nextcord.ext import commands , tasks
 import requests
 
@@ -24,13 +22,17 @@ client = Bot(command_prefix = "!", intents = intents)
 
 @client.event
 async def on_message(message):
-    if message.author.id in usersn and message.channel.type == nextcord.ChannelType.private:
-        data = requests.get(f"https://api.antisniper.net/v2/convert/hypixel?key=bc3ba89b-2a57-46d3-af90-733cc3b439a5&player={message}").json()
+    if message.author.id in users and message.channel.type == nextcord.ChannelType.private:
+        data = requests.get(f"https://api.antisniper.net/v2/other/denick?key=bc3ba89b-2a57-46d3-af90-733cc3b439a5&nick={message.content}").json()
+        print(data)
         await message.channel.send(data["success"])
         if data["success"]:
-            await interaction.channel.send(data["real_nick"])
+            await message.channel.send(data["real_nick"])
             if data["real_nick"]:
-                await message.channel.send(f"IGN: {data['results'][0]['ign']}\nMethod: {data['results'][0]['method']}\nPercentage: {data['results'][0]['percentage']}\nFirst Detected: <t:{data['results'][0]['first_detected']}>\nLast Seen: <t:{data['results'][0]['last_seen']}>")
+                if len(data["results"]) == 0:
+                    await message.channel.send("No data found")
+                else:
+                    await message.channel.send(f"IGN: {data['results'][0]['ign']}\nMethod: {data['results'][0]['method']}\nPercentage: {data['results'][0]['percent']}\nFirst Detected: <t:{data['results'][0]['first_detected']}>\nLast Seen: <t:{data['results'][0]['last_seen']}>")
                 
 
 client.run("MTA4Nzg1NjkzMTEzMTExMzU0Mg.GZadzx.L1v4f52mTRzd271MWtX1pecre4B245eGnjY6oc")
