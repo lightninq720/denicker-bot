b50730c803ae1886f0d6e40ff4bf04bb943ac071
_lightninq <82113835+lightninq720@users.noreply.github.com>
2023-04-16 20:41:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 from nextcord.ext import commands , tasks
 import requests
 
-users = [710044207670231061, 434603846120112130, 326065974950363136]
+TOKEN = ""
+KEY = ""
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -22,12 +23,12 @@ client = Bot(command_prefix = "!", intents = intents)
 
 @client.event
 async def on_message(message):
-    if message.author.id in users and message.channel.type == nextcord.ChannelType.private:
-        data = requests.get(f"https://api.antisniper.net/v2/other/denick?key=bc3ba89b-2a57-46d3-af90-733cc3b439a5&nick={message.content}").json()
+    if message.channel.type == nextcord.ChannelType.private:
+        data = requests.get(f"https://api.antisniper.net/v2/other/denick?key={KEY}&nick={message.content}").json()
         print(data)
-        await message.channel.send(data["success"])
+        await message.channel.send("Request Success:",data["success"])
         if data["success"]:
-            await message.channel.send(data["real_nick"])
+            await message.channel.send("Is Nick:",data["real_nick"])
             if data["real_nick"]:
                 if len(data["results"]) == 0:
                     await message.channel.send("No data found")
@@ -35,4 +36,4 @@ async def on_message(message):
                     await message.channel.send(f"IGN: {data['results'][0]['ign']}\nMethod: {data['results'][0]['method']}\nPercentage: {data['results'][0]['percent']}\nFirst Detected: <t:{data['results'][0]['first_detected']}>\nLast Seen: <t:{data['results'][0]['last_seen']}>")
                 
 
-client.run("MTA4Nzg1NjkzMTEzMTExMzU0Mg.GZadzx.L1v4f52mTRzd271MWtX1pecre4B245eGnjY6oc")
+    client.run(TOKEN)
